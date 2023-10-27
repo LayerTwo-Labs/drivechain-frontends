e85f580f5a291f2d189922cba61a06aa5e5a8539
Bjørn Olav <bo@jalborg.com>
2023-10-27 11:57:47 +0200

    config: introduce class for getting runtime args

---
+++ lib/config/runtime_args.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+// A class where you should put runtime arguments
+abstract class RuntimeArgs {
+  static bool get isInTest =>
+      Platform.environment.containsKey('FLUTTER_TEST') ||
+      const String.fromEnvironment('FLUTTER_TEST').isNotEmpty;
+}
