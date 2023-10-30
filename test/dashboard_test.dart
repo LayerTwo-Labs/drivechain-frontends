28d68ba1765978429411f8825c4deefa2983c52e
Bjørn Olav <bo@jalborg.com>
2023-10-30 08:43:07 +0100

    tabs/dashboard: add sidechain balance to dashboard
    also add a test for verifying the balance show up as expected

--- test/dashboard_test.dart
+++ test/dashboard_test.dart
@@ -6,21 +6,33 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:sidesail/pages/tabs/dashboard_tab_page.dart';
+import 'package:sidesail/providers/balance_provider.dart';
+import 'package:sidesail/rpc/rpc.dart';
 
+import 'mocks/rpc_mock.dart';
 import 'test_utils.dart';
 
 void main() {
-  testWidgets('RPC submit smoke test', (WidgetTester tester) async {
+  setUpAll(() async {
+    GetIt.I.registerLazySingleton<RPC>(() => MockRPC());
+
+    final balanceProvider = BalanceProvider();
+    GetIt.I.registerLazySingleton<BalanceProvider>(() => balanceProvider);
+    // don't start test until balance is fetched
+    await balanceProvider.fetch();
+  });
+  testWidgets('can render and show balance', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpSailPage(
       const DashboardTabPage(),
     );
+    await tester.pumpAndSettle();
 
     // Verify that there's a submit button.
     expect(find.text('Submit'), findsOneWidget);
     expect(find.text('SideSail'), findsOneWidget);
-
-    // TODO: something more meaningful. Would have to mock the RPC interfaces
+    expect(find.text('Your sidechain balance: 1.0 SBTC'), findsOneWidget);
   });
 }
