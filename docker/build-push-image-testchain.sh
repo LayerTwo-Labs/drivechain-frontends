d5087019d77273067bbff193fe4b78dd8d911ac3
Torkel Rogstad <torkel@rogstad.io>
2023-10-30 08:35:06 +0100

    dev: update testchain docker image
    A call to the testchain build script will fetch the most recent master
    commit, and build that. This ensures that builds will be new every time
    master updates.

--- docker/build-push-image-testchain.sh
+++ docker/build-push-image-testchain.sh
@@ -1,2 +1,15 @@
-docker buildx build -t barebitcoin/testchain --file Dockerfile.testchain . 
+set -e
+set -o pipefail
+
+REPO=LayerTwo-Labs/testchain
+MAIN_BRANCH=testchain
+
+TESTCHAIN_VERSION=$(gh api repos/$REPO/git/ref/heads/$MAIN_BRANCH | jq --raw-output .object.sha)
+
+echo Building Docker image based off SHA $TESTCHAIN_VERSION
+
+docker buildx build \
+    --build-arg TESTCHAIN_VERSION=$TESTCHAIN_VERSION \
+    -t barebitcoin/testchain \
+    --file Dockerfile.testchain . 
 docker push barebitcoin/testchain
