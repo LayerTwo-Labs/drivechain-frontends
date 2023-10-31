adb40ffcd07b74daa6d09ef441a7a1cc1aae4c72
Torkel Rogstad <torkel@rogstad.io>
2023-10-31 09:05:51 +0100

    rpc: add configuration functionality
    Instead of hard coded credentials, add functionality for reading
    credentials from file. Supports cookie-based auth.

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -2,14 +2,17 @@ import 'package:get_it/get_it.dart';
 import 'package:sidesail/providers/balance_provider.dart';
 import 'package:sidesail/routing/router.dart';
 import 'package:sidesail/rpc/rpc.dart';
+import 'package:sidesail/rpc/rpc_config.dart';
 import 'package:sidesail/storage/client_settings.dart';
 import 'package:sidesail/storage/secure_store.dart';
 
 // register all global dependencies, for use in views, or in view models
 // each dependency can only be registered once
 Future<void> initGetitDependencies() async {
+  // TODO: this can throw an error. How do we display that to the user?
+  final rpcConfig = await readRpcConfig();
   GetIt.I.registerLazySingleton<RPC>(
-    () => RPCLive(),
+    () => RPCLive(rpcConfig),
   );
 
   GetIt.I.registerLazySingleton<AppRouter>(
