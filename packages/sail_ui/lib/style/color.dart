ab33858907df53567aaa369627a2668a51369ab8
Bjørn Olav <bo@jalborg.com>
2023-11-01 10:18:01 +0100

    sail_ui/color: remove unused colors, introduce dark/light split
    I find it very messy and hard to reason about mixing dark and light
    in the color scheme. going forward, colors will be defined as dark/light
    in the color scheme

--- packages/sail_ui/lib/style/color.dart
+++ packages/sail_ui/lib/style/color.dart
@@ -4,126 +4,88 @@ import 'package:sail_ui/sail_ui.dart';
 class SailColor {
   final Color background;
   final Color backgroundSecondary;
+  final Color backgroundActionModal;
+
   final Color formField;
   final Color formFieldBorder;
-  final Color card;
   final Color divider;
   final Color shadow;
-  final Color highlightedCard;
-  final Color highlightedCardText;
   final Color text;
-  final Color textLowerContrast;
   final Color textSecondary;
   final Color textTertiary;
-  final Color label;
-  final Color sectionTitle;
+  final Color textHint;
   final Color orange;
   final Color disabledPrimaryButton;
-  final Color primaryButtonText;
   final Color icon;
   final Color iconHighlighted;
-  final Color iconTab;
-  final Color lowContrastText;
-  final Color highlightedPillBackground;
-  final Color highlightedPillText;
-  final Color chipCard;
-  final Color blue;
   final Color error;
-  final Color green;
+
+  final Color actionHeader;
+  final Color chip;
 
   SailColor({
     required this.background,
     required this.backgroundSecondary,
+    required this.backgroundActionModal,
     required this.formField,
     required this.formFieldBorder,
-    required this.card,
     required this.divider,
     required this.shadow,
-    required this.highlightedCard,
-    required this.highlightedCardText,
     required this.text,
-    required this.textLowerContrast,
     required this.textSecondary,
     required this.textTertiary,
-    required this.label,
-    required this.sectionTitle,
+    required this.textHint,
     required this.orange,
     required this.disabledPrimaryButton,
-    required this.primaryButtonText,
     required this.iconHighlighted,
-    required this.iconTab,
-    required this.lowContrastText,
     required this.icon,
-    required this.highlightedPillBackground,
-    required this.highlightedPillText,
-    required this.chipCard,
-    required this.blue,
     required this.error,
-    required this.green,
+    required this.actionHeader,
+    required this.chip,
   });
 
   factory SailColor.lightTheme() {
     return SailColor(
       background: SailColorScheme.white,
       backgroundSecondary: SailColorScheme.whiteDark,
+      backgroundActionModal: SailColorScheme.greyMiddle,
       formField: SailColorScheme.whiteDark,
       formFieldBorder: SailColorScheme.greyMiddle,
       divider: SailColorScheme.greyLight,
       shadow: SailColorScheme.black.withOpacity(0.18),
-      card: SailColorScheme.white,
-      highlightedCard: SailColorScheme.black,
-      highlightedCardText: SailColorScheme.white,
       text: SailColorScheme.black,
-      textLowerContrast: SailColorScheme.greyDark,
       textSecondary: SailColorScheme.greyDark,
       textTertiary: SailColorScheme.greyMiddle,
-      label: SailColorScheme.greyDark,
-      sectionTitle: const Color.fromARGB(255, 102, 102, 102),
+      textHint: SailColorScheme.greyMiddle,
       disabledPrimaryButton: SailColorScheme.orange.withOpacity(0.5),
-      primaryButtonText: SailColorScheme.white,
       iconHighlighted: SailColorScheme.orange,
-      iconTab: SailColorScheme.greyMiddle,
-      lowContrastText: const Color.fromARGB(255, 141, 141, 141),
       icon: SailColorScheme.greyMiddle,
-      highlightedPillBackground: SailColorScheme.superLightGreen,
-      highlightedPillText: SailColorScheme.mediumDarkGreen,
-      chipCard: SailColorScheme.whiteDark,
       orange: SailColorScheme.orange,
-      blue: SailColorScheme.blue,
       error: SailColorScheme.pink,
-      green: SailColorScheme.green,
+      actionHeader: SailColorScheme.whiteDark,
+      chip: SailColorScheme.greyMiddle,
     );
   }
   factory SailColor.darkTheme() {
     return SailColor(
-      background: SailColorScheme.black,
+      background: SailColorScheme.darkBackground,
       backgroundSecondary: SailColorScheme.blackLight,
+      backgroundActionModal: SailColorScheme.darkActionModalBackground,
+      actionHeader: SailColorScheme.darkActionHeader,
       formField: SailColorScheme.whiteDark,
       formFieldBorder: SailColorScheme.blackLight,
-      card: SailColorScheme.black,
-      divider: SailColorScheme.greyLight,
+      divider: SailColorScheme.darkDivider,
       shadow: SailColorScheme.greyLight.withOpacity(0.18),
-      highlightedCard: SailColorScheme.white,
-      highlightedCardText: SailColorScheme.black,
-      text: SailColorScheme.white,
-      textLowerContrast: SailColorScheme.white,
-      textSecondary: SailColorScheme.white,
+      text: SailColorScheme.whiteLight,
+      textSecondary: SailColorScheme.darkTextSecondary,
       textTertiary: SailColorScheme.greyMiddle,
-      label: SailColorScheme.greyDark,
-      sectionTitle: SailColorScheme.white,
+      textHint: SailColorScheme.darkTextHint,
       orange: SailColorScheme.orange,
       disabledPrimaryButton: SailColorScheme.orange.withOpacity(0.4),
-      primaryButtonText: SailColorScheme.black,
-      lowContrastText: const Color.fromARGB(255, 130, 130, 130),
       iconHighlighted: SailColorScheme.orange,
-      iconTab: SailColorScheme.greyDark,
       icon: SailColorScheme.greyMiddle,
-      highlightedPillBackground: SailColorScheme.mediumGreen,
-      highlightedPillText: SailColorScheme.black,
-      chipCard: const Color.fromARGB(255, 23, 23, 23),
-      blue: SailColorScheme.blue,
       error: SailColorScheme.pink,
-      green: SailColorScheme.green,
+      chip: SailColorScheme.darkChip,
     );
   }
 }
--- packages/sail_ui/lib/style/color_scheme.dart
+++ packages/sail_ui/lib/style/color_scheme.dart
@@ -2,10 +2,12 @@ import 'dart:ui';
 
 /// References the color scheme
 abstract class SailColorScheme {
+  // both theme colors
   static const blue = Color(0xff16A1F6);
   static const pinkLight = Color(0xffFEC1D5);
   static const pink = Color(0xffFF0089);
   static const red = Color(0xffFF0000);
+  static const black = Color(0xff000000);
   static const greenLight = Color(0xffB5E5AE);
   static const green = Color(0xff2AB517);
   static const orangeLight = Color(0xffFEEDD8);
@@ -14,13 +16,24 @@ abstract class SailColorScheme {
   static const whiteDark = Color(0xffF5F5F5);
   static const greyDark = Color.fromRGBO(115, 115, 115, 1);
   static const greyMiddle = Color(0xffC1C1C1);
-  static const greyLight = Color(0xffE0E0E0);
-  static const black = Color(0xff000000);
+  static const greyLight = Color(0xff707070);
   static const blackLight = Color.fromARGB(255, 18, 18, 18);
-
+  static const blackTertiary = Color(0xff21232E);
   static const superLightGreen = Color(0xffEEF8ED);
   static const lightGreen = Color(0xffEEF8ED);
   static const mediumGreen = Color(0xff5CBE45);
   static const mediumDarkGreen = Color(0xff59B23A);
   static const formFieldGrey = Color(0xffE9EBF0);
+  static const whiteLight = Color(0xffEEEFFC);
+
+  // dark theme colors
+  static const darkBackground = Color(0xff181921);
+  static const darkActionHeader = Color(0xff21232E);
+  static const darkDivider = Color(0xff212234);
+  static const darkChip = Color.fromRGBO(124, 124, 164, 0.1); // #7C7CA4
+  static const darkTextSecondary = Color(0xffD2D3E0);
+  // modal colors
+  static const darkActionModalBackground = Color.fromRGBO(29, 30, 43, 1);
+  static const darkTextHint = Color(0xff4D4F69);
+  // light theme colors
 }
