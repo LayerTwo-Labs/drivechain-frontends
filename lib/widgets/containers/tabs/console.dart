e9854f26ea8efe68f960dfd5ac49273002b072ac
Bjørn Olav <bo@jalborg.com>
2023-11-17 08:49:23 +0100

    containers/console: redesign slightly

--- lib/widgets/containers/tabs/console.dart
+++ lib/widgets/containers/tabs/console.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sail_ui/sail_ui.dart';
 import 'package:sail_ui/theme/theme.dart';
+import 'package:sail_ui/widgets/core/sail_snackbar.dart';
 import 'package:sail_ui/widgets/core/sail_text.dart';
 import 'package:sidesail/logger.dart';
 import 'package:sidesail/rpc/rpc_sidechain.dart';
-import 'package:sidesail/widgets/containers/tabs/dashboard_tab_widgets.dart';
 
 class RPCWidget extends StatefulWidget {
   final List<String> rpcMethods;
@@ -94,90 +95,103 @@ class RPCWidgetState extends State<RPCWidget> {
   @override
   Widget build(BuildContext context) {
     final theme = SailTheme.of(context);
+    var screenSize = MediaQuery.of(context).size;
 
-    return SailColumn(
-      spacing: SailStyleValues.padding08,
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: Autocomplete<String>(
-                optionsBuilder: (TextEditingValue textEditingValue) {
-                  if (textEditingValue.text == '') {
-                    return widget.rpcMethods;
-                  }
-                  return widget.rpcMethods.where((String option) {
-                    return option.contains(textEditingValue.text.toLowerCase());
-                  });
-                },
-                optionsMaxHeight: 500,
-                fieldViewBuilder: (context, textEditingController, focusNode, onFieldSubmitted) {
-                  return SailTextField(
-                    controller: textEditingController,
-                    label: 'RPC command',
-                    prefix: '> ',
-                    hintText: 'Enter rpc command here',
-                    focusNode: focusNode,
-                    onSubmitted: _handleSubmit,
-                    suffixWidget: SailTextButton(
-                      label: 'Submit',
-                      onPressed: () {
-                        _handleSubmit(textEditingController.text);
-                      },
-                    ),
-                  );
-                },
-                optionsViewBuilder: (context, onSelected, options) {
-                  return Scaffold(
-                    backgroundColor: theme.colors.background,
-                    body: Padding(
-                      padding: const EdgeInsets.only(left: SailStyleValues.padding30),
-                      child: SingleChildScrollView(
-                        physics: const BouncingScrollPhysics(),
-                        child: SailColumn(
-                          spacing: SailStyleValues.padding08,
-                          withDivider: true,
-                          children: [
-                            for (final opt in options)
-                              SailScaleButton(
-                                onPressed: () => onSelected(opt),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: SailStyleValues.padding10,
-                                    horizontal: SailStyleValues.padding10,
+    return SizedBox(
+      height: screenSize.height * 0.7,
+      child: SailColumn(
+        spacing: SailStyleValues.padding08,
+        children: [
+          Expanded(
+            child: SailBorder(
+              child: ListView.builder(
+                shrinkWrap: true,
+                reverse: true,
+                physics: const BouncingScrollPhysics(),
+                itemCount: results.length,
+                itemBuilder: (context, index) => ResultView(
+                  key: ValueKey<int>(results[index].id),
+                  result: results[index],
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Autocomplete<String>(
+                  optionsBuilder: (TextEditingValue textEditingValue) {
+                    if (textEditingValue.text == '') {
+                      return widget.rpcMethods;
+                    }
+                    return widget.rpcMethods.where((String option) {
+                      return option.contains(textEditingValue.text.toLowerCase());
+                    });
+                  },
+                  optionsMaxHeight: 500,
+                  fieldViewBuilder: (context, textEditingController, focusNode, onFieldSubmitted) {
+                    return SailTextField(
+                      controller: textEditingController,
+                      label: 'RPC command',
+                      prefixWidget: SailText.secondary12('> ', customColor: getCaretColor(results.firstOrNull)),
+                      hintText: 'Enter rpc command here',
+                      focusNode: focusNode,
+                      onSubmitted: _handleSubmit,
+                      suffixWidget: SailTextButton(
+                        label: 'Submit',
+                        onPressed: () {
+                          _handleSubmit(textEditingController.text);
+                        },
+                      ),
+                    );
+                  },
+                  optionsViewBuilder: (context, onSelected, options) {
+                    return Scaffold(
+                      backgroundColor: theme.colors.background,
+                      body: Padding(
+                        padding: const EdgeInsets.only(left: SailStyleValues.padding30),
+                        child: SingleChildScrollView(
+                          physics: const BouncingScrollPhysics(),
+                          child: SailColumn(
+                            spacing: SailStyleValues.padding08,
+                            withDivider: true,
+                            children: [
+                              for (final opt in options)
+                                SailScaleButton(
+                                  onPressed: () => onSelected(opt),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: SailStyleValues.padding10,
+                                      horizontal: SailStyleValues.padding10,
+                                    ),
+                                    child: SailText.primary12(opt),
                                   ),
-                                  child: SailText.primary12(opt),
                                 ),
-                              ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
-                onSelected: (String selection) {
-                  _handleSubmit(selection);
-                },
+                    );
+                  },
+                  onSelected: (String selection) {},
+                ),
               ),
-            ),
-          ],
-        ),
-        DashboardGroup(
-          title: 'Responses',
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: results.length,
-              itemBuilder: (context, index) => ResultView(
-                key: ValueKey<int>(results[index].id),
-                result: results[index],
-              ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
+  }
+
+  Color? getCaretColor(Result? result) {
+    if (result == null) {
+      return null;
+    }
+
+    if (result.success != null) {
+      return SailColorScheme.green;
+    }
+    return SailColorScheme.red;
   }
 }
 
@@ -195,19 +209,66 @@ class ResultView extends StatelessWidget {
       ),
       child: SailColumn(
         spacing: SailStyleValues.padding08,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          SingleValueContainer(
-            width: 0,
+          ConsoleResponse(
             icon: result.error == null
                 ? SailSVG.icon(SailSVGAsset.iconSuccess, width: 13)
                 : SailSVG.icon(SailSVGAsset.iconFailed, width: 13),
-            copyable: true,
-            customCopyValue: result.error ?? result.success,
-            value: '> ${result.command}\n${result.error ?? result.success}',
+            cmd: result.command,
+            response: result.error ?? result.success ?? '',
           ),
         ],
       ),
     );
+  }
+}
+
+class ConsoleResponse extends StatelessWidget {
+  final String cmd;
+  final String response;
+  final Widget icon;
+
+  const ConsoleResponse({
+    super.key,
+    required this.cmd,
+    required this.response,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SailRow(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: SailStyleValues.padding08,
+      children: [
+        icon,
+        Expanded(
+          child: SailColumn(
+            spacing: 0,
+            children: [
+              SailText.primary12('> $cmd', customColor: SailColorScheme.blue),
+              SailScaleButton(
+                onPressed: () {
+                  Clipboard.setData(ClipboardData(text: response));
+                  showSnackBar(context, 'Copied ${copyLabel()}');
+                },
+                child: SailText.primary12(response),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  String copyLabel() {
+    final textToShow = response;
+    if (textToShow.length > 50) {
+      return '${textToShow.substring(0, 50)}...';
+    }
+
+    return textToShow;
   }
 }
