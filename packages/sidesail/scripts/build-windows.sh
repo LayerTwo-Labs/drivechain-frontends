7bfd995f7c4e6c0f5ee7a0571c61919864518cd3
Torkel Rogstad <torkel@rogstad.io>
2023-12-24 00:22:32 +0100

    build: update windows script

--- packages/sidesail/scripts/build-windows.sh
+++ packages/sidesail/scripts/build-windows.sh
@@ -1,5 +1,14 @@
 set -e 
 
+app_name="$1"
+# Check if exactly one argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 app_name"
+    exit 1
+fi
+
+lower_app_name=$(echo "$app_name" | tr '[:upper:]' '[:lower:]')
+
 # Dirty hack: we're running the big build script
 # from within bash in WSL, and we need to go back
 # to Windows land when doing the Flutter build.
@@ -11,3 +20,8 @@ clean_cmd="flutter clean"
 build_cmd="dart run msix:create"
 
 powershell.exe -Command "& {$clean_cmd; $build_cmd; exit}"
+
+zip_name=$lower_app_name-win64.zip
+mkdir -p release
+
+powershell.exe -Command "Compress-Archive -Force build\windows\runner\Release\* release/$zip_name"
