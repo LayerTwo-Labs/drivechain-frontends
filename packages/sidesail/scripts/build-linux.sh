d80eaee1683a15703b20d635db3f114dbf055c66
Torkel Rogstad <torkel@rogstad.io>
2024-01-04 08:12:21 +0100

    scripts: update linux build

--- packages/sidesail/scripts/build-linux.sh
+++ packages/sidesail/scripts/build-linux.sh
@@ -1,1 +1,23 @@
+set -e
+
+app_name="$1"
+# Check if exactly one argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 app_name"
+    exit 1
+fi
+
+lower_app_name=$(echo "$app_name" | tr '[:upper:]' '[:lower:]')
+
 flutter build linux --dart-define-from-file=build-vars.env
+
+old_cwd=$PWD
+cd build/linux/x64/release/bundle
+
+zip_name=$lower_app_name-x86_64-linux-gnu.zip 
+echo Zipping into $zip_name
+
+zip -q -r $zip_name *
+
+mkdir -p $old_cwd/release
+cp $zip_name $old_cwd/release
