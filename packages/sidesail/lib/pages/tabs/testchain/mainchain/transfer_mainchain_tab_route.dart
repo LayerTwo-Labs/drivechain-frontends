ae25ddc8f31f496cc8cca36a94861b049e3d14e7
Bjørn Olav <bo@jalborg.com>
2024-01-18 16:37:51 +0100

    tabs/mainchain: make it possible to send/receive directly on parent chain

--- packages/sidesail/lib/pages/tabs/testchain/mainchain/transfer_mainchain_tab_route.dart
+++ packages/sidesail/lib/pages/tabs/testchain/mainchain/transfer_mainchain_tab_route.dart
@@ -1,15 +1,21 @@
+import 'dart:async';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 import 'package:sail_ui/sail_ui.dart';
+import 'package:sail_ui/theme/theme.dart';
 import 'package:sail_ui/widgets/core/sail_text.dart';
 import 'package:sidesail/bitcoin.dart';
 import 'package:sidesail/config/sidechains.dart';
+import 'package:sidesail/providers/balance_provider.dart';
 import 'package:sidesail/providers/transactions_provider.dart';
 import 'package:sidesail/routing/router.dart';
 import 'package:sidesail/rpc/models/utxo.dart';
+import 'package:sidesail/rpc/rpc_mainchain.dart';
 import 'package:sidesail/rpc/rpc_sidechain.dart';
+import 'package:sidesail/widgets/containers/dashboard_action_modal.dart';
 import 'package:sidesail/widgets/containers/tabs/dashboard_tab_widgets.dart';
 import 'package:sidesail/widgets/containers/tabs/transfer_mainchain_tab_widgets.dart';
 import 'package:stacked/stacked.dart';
@@ -22,6 +28,8 @@ class TransferMainchainTabPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = SailTheme.of(context);
+
     return ViewModelBuilder.reactive(
       viewModelBuilder: () => TransferMainchainTabViewModel(),
       builder: ((context, viewModel, child) {
@@ -29,30 +37,77 @@ class TransferMainchainTabPage extends StatelessWidget {
           body: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.only(bottom: SailStyleValues.padding30),
-              child: Column(
+              child: SailColumn(
+                spacing: SailStyleValues.padding30,
                 children: [
-                  DashboardGroup(
-                    title: 'Actions',
+                  SailRow(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 0,
                     children: [
-                      ActionTile(
-                        title: 'Peg-out to parent chain',
-                        category: Category.mainchain,
-                        icon: Icons.remove,
-                        onTap: () {
-                          viewModel.pegOut(context);
-                        },
+                      Flexible(
+                        child: SailColumn(
+                          spacing: SailStyleValues.padding30,
+                          children: [
+                            DashboardGroup(
+                              title: 'Actions',
+                              children: [
+                                ActionTile(
+                                  title: 'Send on parent chain',
+                                  category: Category.mainchain,
+                                  icon: Icons.remove,
+                                  onTap: () {
+                                    viewModel.send(context);
+                                  },
+                                ),
+                                ActionTile(
+                                  title: 'Receive on parent chain',
+                                  category: Category.mainchain,
+                                  icon: Icons.add,
+                                  onTap: () {
+                                    viewModel.receive(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                      ActionTile(
-                        title: 'Peg-in from parent chain',
-                        category: Category.mainchain,
-                        icon: Icons.add,
-                        onTap: () {
-                          viewModel.pegIn(context);
-                        },
+                      VerticalDivider(
+                        width: 1,
+                        thickness: 1,
+                        color: theme.colors.divider,
+                      ),
+                      Flexible(
+                        child: SailColumn(
+                          spacing: SailStyleValues.padding30,
+                          children: [
+                            DashboardGroup(
+                              title: 'Actions',
+                              children: [
+                                ActionTile(
+                                  title: 'Peg-out to parent chain',
+                                  category: Category.mainchain,
+                                  icon: Icons.remove,
+                                  onTap: () {
+                                    viewModel.pegOut(context);
+                                  },
+                                ),
+                                ActionTile(
+                                  title: 'Peg-in from parent chain',
+                                  category: Category.mainchain,
+                                  icon: Icons.add,
+                                  onTap: () {
+                                    viewModel.pegIn(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                  const SailSpacing(SailStyleValues.padding30),
                   DashboardGroup(
                     title: 'UTXOs',
                     widgetTrailing: SailText.secondary13(viewModel.utxos.length.toString()),
@@ -89,6 +144,7 @@ class TransferMainchainTabViewModel extends BaseViewModel {
   final log = Logger(level: Level.debug);
   TransactionsProvider get _transactionsProvider => GetIt.I.get<TransactionsProvider>();
   SidechainContainer get _sidechain => GetIt.I.get<SidechainContainer>();
+  MainchainRPC get _mainRPC => GetIt.I.get<MainchainRPC>();
 
   List<UTXO> get utxos => _transactionsProvider.unspentMainchainUTXOs;
 
@@ -130,9 +186,211 @@ class TransferMainchainTabViewModel extends BaseViewModel {
     );
   }
 
+  void send(BuildContext context) async {
+    await showThemedDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const SendOnParentChainAction();
+      },
+    );
+  }
+
+  void receive(BuildContext context) async {
+    await showThemedDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return ReceiveAction(
+          customTitle: 'Receive on parent chain',
+          customReceiveAction: () async {
+            return await _mainRPC.getNewAddress();
+          },
+        );
+      },
+    );
+  }
+
   @override
   void dispose() {
     super.dispose();
     _transactionsProvider.removeListener(notifyListeners);
+  }
+}
+
+class SendOnParentChainAction extends StatelessWidget {
+  final Future<String> Function(String address, double amount)? customSendAction;
+
+  const SendOnParentChainAction({
+    super.key,
+    this.customSendAction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => SendViewModel(customSendAction: customSendAction),
+      builder: ((context, viewModel, child) {
+        return DashboardActionModal(
+          'Send on parent chain',
+          endActionButton: SailButton.primary(
+            'Execute send',
+            disabled: viewModel.bitcoinAddressController.text.isEmpty || viewModel.bitcoinAmountController.text.isEmpty,
+            loading: viewModel.isBusy,
+            size: ButtonSize.regular,
+            onPressed: () async {
+              viewModel.executeSendOnParentChain(context);
+            },
+          ),
+          children: [
+            LargeEmbeddedInput(
+              controller: viewModel.bitcoinAddressController,
+              hintText: 'Enter a parent chain address',
+              autofocus: true,
+            ),
+            LargeEmbeddedInput(
+              controller: viewModel.bitcoinAmountController,
+              hintText: 'Enter a BTC-amount',
+              suffixText: viewModel.ticker,
+              bitcoinInput: true,
+            ),
+            StaticActionField(
+              label: 'Fee',
+              value: '${(viewModel.expectedFee ?? 0).toStringAsFixed(8)} ${viewModel.ticker}',
+            ),
+            StaticActionField(
+              label: 'Total amount',
+              value: '${viewModel.totalBitcoinAmount} ${viewModel.ticker}',
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
+
+class SendViewModel extends BaseViewModel {
+  final log = Logger(level: Level.debug);
+  SidechainContainer get _sidechainContainer => GetIt.I.get<SidechainContainer>();
+
+  BalanceProvider get _balanceProvider => GetIt.I.get<BalanceProvider>();
+  TransactionsProvider get _transactionsProvider => GetIt.I.get<TransactionsProvider>();
+  AppRouter get _router => GetIt.I.get<AppRouter>();
+  MainchainRPC get _rpc => GetIt.I.get<MainchainRPC>();
+
+  final bitcoinAddressController = TextEditingController();
+  final bitcoinAmountController = TextEditingController();
+  String get totalBitcoinAmount =>
+      ((double.tryParse(bitcoinAmountController.text) ?? 0) + (expectedFee ?? 0)).toStringAsFixed(8);
+  String get ticker => _sidechainContainer.rpc.chain.ticker;
+
+  double? expectedFee;
+  double? get sendAmount => double.tryParse(bitcoinAmountController.text);
+
+  final Future<String> Function(String address, double amount)? customSendAction;
+
+  SendViewModel({this.customSendAction}) {
+    bitcoinAddressController.addListener(notifyListeners);
+    bitcoinAmountController.addListener(notifyListeners);
+    init();
+  }
+
+  void init() async {
+    await estimateFee();
+  }
+
+  void executeSendOnParentChain(BuildContext context) async {
+    setBusy(true);
+    onParentSend(context);
+    setBusy(false);
+  }
+
+  Future<void> estimateFee() async {
+    final estimate = await _rpc.estimateFee();
+    expectedFee = estimate;
+    notifyListeners();
+  }
+
+  void onParentSend(BuildContext context) async {
+    if (sendAmount == null) {
+      log.e('withdrawal amount was empty');
+      return;
+    }
+    if (expectedFee == null) {
+      log.e('sidechain fee was empty');
+      return;
+    }
+
+    final address = bitcoinAddressController.text;
+
+    // Because the function is async, the view might disappear/unmount
+    // by the time it's used. The linter doesn't like that, and wants
+    // you to check whether the view is mounted before using it
+    if (!context.mounted) {
+      return;
+    }
+
+    _doSend(
+      context,
+      address,
+      sendAmount!,
+    );
+  }
+
+  void _doSend(
+    BuildContext context,
+    String address,
+    double amount,
+  ) async {
+    log.i(
+      'doing parent chain transfer: $amount $ticker to $address with $expectedFee SC fee',
+    );
+
+    try {
+      final sendTXID = await _send(address, amount);
+      log.i('sent parent chain transfer txid: $sendTXID sendTXID');
+
+      // refresh balance, but don't await, so dialog is showed instantly
+      unawaited(_balanceProvider.fetch());
+      // refresh transactions, but don't await, so dialog is showed instantly
+      unawaited(_transactionsProvider.fetch());
+
+      if (!context.mounted) {
+        return;
+      }
+
+      await successDialog(
+        context: context,
+        action: 'Send on parent chain',
+        title: 'You sent $amount $ticker to $address',
+        subtitle: 'TXID: $sendTXID',
+      );
+      // also pop the info modal
+      await _router.pop();
+    } catch (error) {
+      if (!context.mounted) {
+        return;
+      }
+
+      await errorDialog(
+        context: context,
+        action: 'Send on parent chain',
+        title: 'Could not execute transfer',
+        subtitle: error.toString(),
+      );
+    }
+  }
+
+  Future<String> _send(
+    String address,
+    double amount,
+  ) async {
+    if (customSendAction != null) {
+      return await customSendAction!(address, amount);
+    }
+
+    return await _rpc.send(
+      address,
+      amount,
+      false,
+    );
   }
 }
