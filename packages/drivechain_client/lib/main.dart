ed6acf1a2d3e5cb0bcc1abf1e1fa29882b6465cd
Arne Molland <arne@molland.sh>
2024-08-26 09:46:18 +0200

    drivechain_client: add sidesail_ui dependencies

--- packages/drivechain_client/lib/main.dart
+++ packages/drivechain_client/lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:drivechain_client/routing/router.dart';
 import 'package:drivechain_client/service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sail_ui/sail_ui.dart';
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+ main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   final log = Logger();
   final router = AppRouter();
+  final prefs = await SharedPreferences.getInstance();
+
+  // Needed for sidesail_ui to work
+  GetIt.I.registerLazySingleton<ClientSettings>(
+    () => ClientSettings(
+      store: Storage(
+        preferences: prefs,
+      ),
+      log: log,
+    ),
+  );
 
   runApp(
     DrivechainService(
