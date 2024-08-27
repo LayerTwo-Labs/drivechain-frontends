4a1f4774423143427236b0101bd2b7158a4678f1
Arne Molland <arne@molland.sh>
2024-08-27 12:08:32 +0200

    drivechain_client: style navigation tabs
    An homage to the Qt application

---
+++ packages/drivechain_client/lib/util/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+extension ColorExtension on Color {
+  Color darken(double amount) {
+    assert(amount >= 0 && amount <= 1, 'Amount must be between 0 and 1');
+    final hsl = HSLColor.fromColor(this);
+    return hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0)).toColor();
+  }
+
+  Color lighten(double amount) {
+    assert(amount >= 0 && amount <= 1, 'Amount must be between 0 and 1');
+    final hsl = HSLColor.fromColor(this);
+    return hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0)).toColor();
+  }
+}
