0217c82447b9af1a170e096e11b8d1c5a61d658f
Torkel Rogstad <torkel@rogstad.io>
2024-09-03 14:19:43 +0200

    faucet: move to docker build + deploy

---
+++ scripts/deploy-faucet.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e 
+
+docker compose --file docker-compose.faucet.yml up --build --wait 
+
+cd ./packages/faucet
+flutter clean
+flutter pub get --enforce-lockfile
+flutter build web
+
+instance=$(gcloud compute instances list --format="value(NAME)" | grep layertwo-labs)
+
+# Clean old stuff
+gcloud compute ssh $instance -- "rm -rf /var/www/drivechain-live/*"
+
+# Copy over new stuff
+gcloud compute scp --recurse ./build/web/* $instance:/var/www/drivechain-live/
