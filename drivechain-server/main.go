cbd48bed9e2f49e3c76c09330277521ffbebd20b
Arne Molland <arne@molland.sh>
2024-09-10 13:39:22 +0200

    drivechain-server: improve `getDataDir` messages, merge swtich cases

--- drivechain-server/main.go
+++ drivechain-server/main.go
@@ -179,6 +179,7 @@ func getDataDir() (string, error) {
 
 	switch runtime.GOOS {
 	case "linux":
+	case "darwin":
 		if xdgDataHome := os.Getenv("XDG_DATA_HOME"); xdgDataHome != "" {
 			dir = filepath.Join(xdgDataHome, appName)
 		} else {
@@ -186,22 +187,20 @@ func getDataDir() (string, error) {
 			if err != nil {
 				return "", err
 			}
-			dir = filepath.Join(home, ".local", "share", appName)
+			if runtime.GOOS == "darwin" {
+				dir = filepath.Join(home, "Library", "Application Support", appName)
+			} else {
+				dir = filepath.Join(home, ".local", "share", appName)
+			}
 		}
-	case "darwin":
-		home, err := os.UserHomeDir()
-		if err != nil {
-			return "", err
-		}
-		dir = filepath.Join(home, "Library", "Application Support", appName)
 	case "windows":
 		appData, ok := os.LookupEnv("APPDATA")
 		if !ok {
-			return "", os.ErrNotExist
+			return "", fmt.Errorf("APPDATA environment variable not set")
 		}
 		dir = filepath.Join(appData, appName)
 	default:
-		return "", os.ErrNotExist
+		return "", fmt.Errorf("unsupported OS: %s", runtime.GOOS)
 	}
 
 	// Ensure the directory exists
