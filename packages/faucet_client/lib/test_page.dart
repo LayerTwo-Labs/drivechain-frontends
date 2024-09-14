287942e4c51b8b004efd436c76d79ed9f44dd6f2
Bjørn Olav <bo@jalborg.com>
2024-09-14 10:05:15 +0200

    faucet_client: fix tests

---
+++ packages/faucet_client/lib/test_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SailTestPage extends StatelessWidget {
+  final Widget child;
+  const SailTestPage({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+}
