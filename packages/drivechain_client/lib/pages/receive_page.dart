2c0bc1fb81d69c3608cf050f2a0c29e7f99228bf
Bjørn Olav <bo@jalborg.com>
2024-09-17 12:10:37 +0200

    drivechain_client/receive: implement initial UI

--- packages/drivechain_client/lib/pages/receive_page.dart
+++ packages/drivechain_client/lib/pages/receive_page.dart
@@ -1,5 +1,10 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/widgets.dart';
+import 'package:drivechain_client/pages/send_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:sail_ui/sail_ui.dart';
+import 'package:stacked/stacked.dart';
 
 @RoutePage()
 class ReceivePage extends StatelessWidget {
@@ -7,6 +12,90 @@ class ReceivePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return QtPage(
+      child: ViewModelBuilder.reactive(
+        viewModelBuilder: () => ReceivePageViewModel(),
+        onViewModelReady: (viewModel) => viewModel.init(),
+        builder: (context, viewModel, child) {
+          return Column(
+            children: [
+              QtContainer(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: QtContainer(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: SailTextField(
+                                    controller: viewModel.addressController,
+                                    hintText: 'A Drivechain address',
+                                    readOnly: true,
+                                  ),
+                                ),
+                                const SizedBox(width: 4.0),
+                                QtIconButton(
+                                  onPressed: () async {
+                                    await Clipboard.setData(ClipboardData(text: viewModel.addressController.text))
+                                        .then((_) {
+                                      if (context.mounted) showSnackBar(context, 'Copied address');
+                                    }).catchError((error) {
+                                      if (context.mounted) showSnackBar(context, 'Could not copy address: $error ');
+                                    });
+                                  },
+                                  icon: const Icon(
+                                    Icons.content_paste_rounded,
+                                    size: 20.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            QtButton(
+                              onPressed: viewModel.generateNewAddress,
+                              child: SailText.primary12('New'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    QrImageView(
+                      data: viewModel.addressController.text,
+                      version: QrVersions.auto,
+                      size: 200.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ReceivePageViewModel extends BaseViewModel {
+  late TextEditingController addressController;
+
+  void init() {
+    addressController = TextEditingController(text: '1KwVGQhtYkrbNgHpSUjUpbrfCGu9WXgZhS');
+  }
+
+  @override
+  void dispose() {
+    addressController.dispose();
+    super.dispose();
+  }
+
+  void generateNewAddress() {
+    addressController.text = 'NewGeneratedAddress';
+    notifyListeners();
   }
 }
