bc08cee74a23ad4eb498728ae02dcbc42ea15215
Bjørn Olav <bo@jalborg.com>
2024-09-18 16:22:04 +0200

    clients/multi: refactor change theme button into sailui
    also add it to drivechain client

---
+++ packages/sail_ui/lib/widgets/buttons/toggle_theme_button.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:sail_ui/sail_ui.dart';
+
+class ToggleThemeButton extends StatefulWidget {
+  const ToggleThemeButton({super.key});
+
+  @override
+  State<ToggleThemeButton> createState() => _ToggleThemeButtonState();
+}
+
+class _ToggleThemeButtonState extends State<ToggleThemeButton> {
+  final _clientSettings = GetIt.I<ClientSettings>();
+
+  _ToggleThemeButtonState();
+
+  SailThemeValues _currentTheme = SailThemeValues.system;
+
+  @override
+  void initState() {
+    super.initState();
+    loadTheme();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final app = SailApp.of(context);
+
+    return SailScaleButton(
+      onPressed: () async {
+        final SailThemeValues nextTheme = _currentTheme.toggleTheme();
+        await setTheme(nextTheme);
+        await app.loadTheme(nextTheme);
+      },
+      child: Tooltip(
+        message: 'Current theme is ${_currentTheme.toReadable()}',
+        child: SailPadding(
+          padding: const EdgeInsets.only(
+            right: SailStyleValues.padding10,
+          ),
+          child: themeIcon(_currentTheme),
+        ),
+      ),
+    );
+  }
+
+  Widget themeIcon(SailThemeValues currentTheme) {
+    SailSVGAsset icon;
+    if (currentTheme == SailThemeValues.system) {
+      icon = SailSVGAsset.iconLightDarkMode;
+    } else if (currentTheme == SailThemeValues.light) {
+      icon = SailSVGAsset.iconLightMode;
+    } else {
+      icon = SailSVGAsset.iconDarkMode;
+    }
+
+    return SailSVG.fromAsset(
+      icon,
+      height: icon == SailSVGAsset.iconLightMode ? 20 : null,
+    );
+  }
+
+  Future<void> setTheme(SailThemeValues newTheme) async {
+    setState(() {
+      _currentTheme = newTheme;
+    });
+    await _clientSettings.setValue(ThemeSetting().withValue(newTheme));
+  }
+
+  Future<void> loadTheme() async {
+    final themeSetting = await _clientSettings.getValue(ThemeSetting());
+    _currentTheme = themeSetting.value;
+    setState(() {});
+  }
+}
