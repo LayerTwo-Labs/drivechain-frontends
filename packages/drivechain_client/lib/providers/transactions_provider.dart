484ede12c6c5a71c3f240258c96a4268d6fa9a97
Bjørn Olav <bo@jalborg.com>
2024-09-21 12:01:53 +0200

    drivechain_client: refetch transactions when balance changes

--- packages/drivechain_client/lib/providers/transactions_provider.dart
+++ packages/drivechain_client/lib/providers/transactions_provider.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:drivechain_client/api.dart';
 import 'package:drivechain_client/gen/wallet/v1/wallet.pbgrpc.dart';
+import 'package:drivechain_client/providers/balance_provider.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 
@@ -9,13 +10,16 @@ import 'package:get_it/get_it.dart';
 // to this class will be notified of changes to new transactions
 class TransactionProvider extends ChangeNotifier {
   API get api => GetIt.I.get<API>();
+  BalanceProvider get balanceProvider => GetIt.I.get<BalanceProvider>();
 
   List<Transaction> walletTransactions = [];
   bool initialized = false;
 
   bool _isFetching = false;
 
-  TransactionProvider();
+  TransactionProvider() {
+    balanceProvider.addListener(fetch);
+  }
 
   // call this function from anywhere to refetch transaction list
   Future<void> fetch() async {
@@ -44,5 +48,11 @@ class TransactionProvider extends ChangeNotifier {
     }
 
     return false;
+  }
+
+  @override
+  void dispose() {
+    balanceProvider.removeListener(fetch);
+    super.dispose();
   }
 }
