9fba044f205c9ca2c325260ed2612848cf46d75d
Bjørn Olav <bo@jalborg.com>
2024-09-24 14:21:54 +0200

    drivechain_client/sidechains page: add list of sidechains

--- packages/drivechain_client/lib/gen/bitcoind/v1/bitcoind.pbgrpc.dart
+++ packages/drivechain_client/lib/gen/bitcoind/v1/bitcoind.pbgrpc.dart
@@ -12,48 +12,60 @@
 import 'dart:async' as $async;
 import 'dart:core' as $core;
 
+import 'package:drivechain_client/gen/bitcoind/v1/bitcoind.pb.dart' as $0;
+import 'package:drivechain_client/gen/google/protobuf/empty.pb.dart' as $1;
 import 'package:grpc/service_api.dart' as $grpc;
 import 'package:protobuf/protobuf.dart' as $pb;
-
-import 'package:drivechain_client/gen/google/protobuf/empty.pb.dart' as $1;
-import 'package:drivechain_client/gen/bitcoind/v1/bitcoind.pb.dart' as $0;
 
 export 'bitcoind.pb.dart';
 
 @$pb.GrpcServiceName('bitcoind.v1.BitcoindService')
 class BitcoindServiceClient extends $grpc.Client {
-  static final _$listUnconfirmedTransactions = $grpc.ClientMethod<$0.ListUnconfirmedTransactionsRequest, $0.ListUnconfirmedTransactionsResponse>(
-      '/bitcoind.v1.BitcoindService/ListUnconfirmedTransactions',
-      ($0.ListUnconfirmedTransactionsRequest value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.ListUnconfirmedTransactionsResponse.fromBuffer(value),);
+  static final _$listUnconfirmedTransactions =
+      $grpc.ClientMethod<$0.ListUnconfirmedTransactionsRequest, $0.ListUnconfirmedTransactionsResponse>(
+    '/bitcoind.v1.BitcoindService/ListUnconfirmedTransactions',
+    ($0.ListUnconfirmedTransactionsRequest value) => value.writeToBuffer(),
+    ($core.List<$core.int> value) => $0.ListUnconfirmedTransactionsResponse.fromBuffer(value),
+  );
   static final _$listRecentBlocks = $grpc.ClientMethod<$0.ListRecentBlocksRequest, $0.ListRecentBlocksResponse>(
-      '/bitcoind.v1.BitcoindService/ListRecentBlocks',
-      ($0.ListRecentBlocksRequest value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.ListRecentBlocksResponse.fromBuffer(value),);
+    '/bitcoind.v1.BitcoindService/ListRecentBlocks',
+    ($0.ListRecentBlocksRequest value) => value.writeToBuffer(),
+    ($core.List<$core.int> value) => $0.ListRecentBlocksResponse.fromBuffer(value),
+  );
   static final _$getBlockchainInfo = $grpc.ClientMethod<$1.Empty, $0.GetBlockchainInfoResponse>(
-      '/bitcoind.v1.BitcoindService/GetBlockchainInfo',
-      ($1.Empty value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.GetBlockchainInfoResponse.fromBuffer(value),);
+    '/bitcoind.v1.BitcoindService/GetBlockchainInfo',
+    ($1.Empty value) => value.writeToBuffer(),
+    ($core.List<$core.int> value) => $0.GetBlockchainInfoResponse.fromBuffer(value),
+  );
   static final _$listPeers = $grpc.ClientMethod<$1.Empty, $0.ListPeersResponse>(
-      '/bitcoind.v1.BitcoindService/ListPeers',
-      ($1.Empty value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.ListPeersResponse.fromBuffer(value),);
+    '/bitcoind.v1.BitcoindService/ListPeers',
+    ($1.Empty value) => value.writeToBuffer(),
+    ($core.List<$core.int> value) => $0.ListPeersResponse.fromBuffer(value),
+  );
   static final _$estimateSmartFee = $grpc.ClientMethod<$0.EstimateSmartFeeRequest, $0.EstimateSmartFeeResponse>(
-      '/bitcoind.v1.BitcoindService/EstimateSmartFee',
-      ($0.EstimateSmartFeeRequest value) => value.writeToBuffer(),
-      ($core.List<$core.int> value) => $0.EstimateSmartFeeResponse.fromBuffer(value),);
+    '/bitcoind.v1.BitcoindService/EstimateSmartFee',
+    ($0.EstimateSmartFeeRequest value) => value.writeToBuffer(),
+    ($core.List<$core.int> value) => $0.EstimateSmartFeeResponse.fromBuffer(value),
+  );
 
-  BitcoindServiceClient($grpc.ClientChannel channel,
-      {$grpc.CallOptions? options,
-      $core.Iterable<$grpc.ClientInterceptor>? interceptors,})
-      : super(channel, options: options,
-        interceptors: interceptors,);
+  BitcoindServiceClient(
+    $grpc.ClientChannel channel, {
+    $grpc.CallOptions? options,
+    $core.Iterable<$grpc.ClientInterceptor>? interceptors,
+  }) : super(
+          channel,
+          options: options,
+          interceptors: interceptors,
+        );
 
-  $grpc.ResponseFuture<$0.ListUnconfirmedTransactionsResponse> listUnconfirmedTransactions($0.ListUnconfirmedTransactionsRequest request, {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$0.ListUnconfirmedTransactionsResponse> listUnconfirmedTransactions(
+      $0.ListUnconfirmedTransactionsRequest request,
+      {$grpc.CallOptions? options,}) {
     return $createUnaryCall(_$listUnconfirmedTransactions, request, options: options);
   }
 
-  $grpc.ResponseFuture<$0.ListRecentBlocksResponse> listRecentBlocks($0.ListRecentBlocksRequest request, {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$0.ListRecentBlocksResponse> listRecentBlocks($0.ListRecentBlocksRequest request,
+      {$grpc.CallOptions? options,}) {
     return $createUnaryCall(_$listRecentBlocks, request, options: options);
   }
 
@@ -65,7 +77,8 @@ class BitcoindServiceClient extends $grpc.Client {
     return $createUnaryCall(_$listPeers, request, options: options);
   }
 
-  $grpc.ResponseFuture<$0.EstimateSmartFeeResponse> estimateSmartFee($0.EstimateSmartFeeRequest request, {$grpc.CallOptions? options}) {
+  $grpc.ResponseFuture<$0.EstimateSmartFeeResponse> estimateSmartFee($0.EstimateSmartFeeRequest request,
+      {$grpc.CallOptions? options,}) {
     return $createUnaryCall(_$estimateSmartFee, request, options: options);
   }
 }
@@ -75,52 +88,70 @@ abstract class BitcoindServiceBase extends $grpc.Service {
   $core.String get $name => 'bitcoind.v1.BitcoindService';
 
   BitcoindServiceBase() {
-    $addMethod($grpc.ServiceMethod<$0.ListUnconfirmedTransactionsRequest, $0.ListUnconfirmedTransactionsResponse>(
+    $addMethod(
+      $grpc.ServiceMethod<$0.ListUnconfirmedTransactionsRequest, $0.ListUnconfirmedTransactionsResponse>(
         'ListUnconfirmedTransactions',
         listUnconfirmedTransactions_Pre,
         false,
         false,
         ($core.List<$core.int> value) => $0.ListUnconfirmedTransactionsRequest.fromBuffer(value),
-        ($0.ListUnconfirmedTransactionsResponse value) => value.writeToBuffer(),),);
-    $addMethod($grpc.ServiceMethod<$0.ListRecentBlocksRequest, $0.ListRecentBlocksResponse>(
+        ($0.ListUnconfirmedTransactionsResponse value) => value.writeToBuffer(),
+      ),
+    );
+    $addMethod(
+      $grpc.ServiceMethod<$0.ListRecentBlocksRequest, $0.ListRecentBlocksResponse>(
         'ListRecentBlocks',
         listRecentBlocks_Pre,
         false,
         false,
         ($core.List<$core.int> value) => $0.ListRecentBlocksRequest.fromBuffer(value),
-        ($0.ListRecentBlocksResponse value) => value.writeToBuffer(),),);
-    $addMethod($grpc.ServiceMethod<$1.Empty, $0.GetBlockchainInfoResponse>(
+        ($0.ListRecentBlocksResponse value) => value.writeToBuffer(),
+      ),
+    );
+    $addMethod(
+      $grpc.ServiceMethod<$1.Empty, $0.GetBlockchainInfoResponse>(
         'GetBlockchainInfo',
         getBlockchainInfo_Pre,
         false,
         false,
         ($core.List<$core.int> value) => $1.Empty.fromBuffer(value),
-        ($0.GetBlockchainInfoResponse value) => value.writeToBuffer(),),);
-    $addMethod($grpc.ServiceMethod<$1.Empty, $0.ListPeersResponse>(
+        ($0.GetBlockchainInfoResponse value) => value.writeToBuffer(),
+      ),
+    );
+    $addMethod(
+      $grpc.ServiceMethod<$1.Empty, $0.ListPeersResponse>(
         'ListPeers',
         listPeers_Pre,
         false,
         false,
         ($core.List<$core.int> value) => $1.Empty.fromBuffer(value),
-        ($0.ListPeersResponse value) => value.writeToBuffer(),),);
-    $addMethod($grpc.ServiceMethod<$0.EstimateSmartFeeRequest, $0.EstimateSmartFeeResponse>(
+        ($0.ListPeersResponse value) => value.writeToBuffer(),
+      ),
+    );
+    $addMethod(
+      $grpc.ServiceMethod<$0.EstimateSmartFeeRequest, $0.EstimateSmartFeeResponse>(
         'EstimateSmartFee',
         estimateSmartFee_Pre,
         false,
         false,
         ($core.List<$core.int> value) => $0.EstimateSmartFeeRequest.fromBuffer(value),
-        ($0.EstimateSmartFeeResponse value) => value.writeToBuffer(),),);
+        ($0.EstimateSmartFeeResponse value) => value.writeToBuffer(),
+      ),
+    );
   }
 
-  $async.Future<$0.ListUnconfirmedTransactionsResponse> listUnconfirmedTransactions_Pre($grpc.ServiceCall call, $async.Future<$0.ListUnconfirmedTransactionsRequest> request) async {
+  $async.Future<$0.ListUnconfirmedTransactionsResponse> listUnconfirmedTransactions_Pre(
+      $grpc.ServiceCall call, $async.Future<$0.ListUnconfirmedTransactionsRequest> request,) async {
     return listUnconfirmedTransactions(call, await request);
   }
 
-  $async.Future<$0.ListRecentBlocksResponse> listRecentBlocks_Pre($grpc.ServiceCall call, $async.Future<$0.ListRecentBlocksRequest> request) async {
+  $async.Future<$0.ListRecentBlocksResponse> listRecentBlocks_Pre(
+      $grpc.ServiceCall call, $async.Future<$0.ListRecentBlocksRequest> request,) async {
     return listRecentBlocks(call, await request);
   }
 
-  $async.Future<$0.GetBlockchainInfoResponse> getBlockchainInfo_Pre($grpc.ServiceCall call, $async.Future<$1.Empty> request) async {
+  $async.Future<$0.GetBlockchainInfoResponse> getBlockchainInfo_Pre(
+      $grpc.ServiceCall call, $async.Future<$1.Empty> request,) async {
     return getBlockchainInfo(call, await request);
   }
 
@@ -128,13 +159,17 @@ abstract class BitcoindServiceBase extends $grpc.Service {
     return listPeers(call, await request);
   }
 
-  $async.Future<$0.EstimateSmartFeeResponse> estimateSmartFee_Pre($grpc.ServiceCall call, $async.Future<$0.EstimateSmartFeeRequest> request) async {
+  $async.Future<$0.EstimateSmartFeeResponse> estimateSmartFee_Pre(
+      $grpc.ServiceCall call, $async.Future<$0.EstimateSmartFeeRequest> request,) async {
     return estimateSmartFee(call, await request);
   }
 
-  $async.Future<$0.ListUnconfirmedTransactionsResponse> listUnconfirmedTransactions($grpc.ServiceCall call, $0.ListUnconfirmedTransactionsRequest request);
-  $async.Future<$0.ListRecentBlocksResponse> listRecentBlocks($grpc.ServiceCall call, $0.ListRecentBlocksRequest request);
+  $async.Future<$0.ListUnconfirmedTransactionsResponse> listUnconfirmedTransactions(
+      $grpc.ServiceCall call, $0.ListUnconfirmedTransactionsRequest request,);
+  $async.Future<$0.ListRecentBlocksResponse> listRecentBlocks(
+      $grpc.ServiceCall call, $0.ListRecentBlocksRequest request,);
   $async.Future<$0.GetBlockchainInfoResponse> getBlockchainInfo($grpc.ServiceCall call, $1.Empty request);
   $async.Future<$0.ListPeersResponse> listPeers($grpc.ServiceCall call, $1.Empty request);
-  $async.Future<$0.EstimateSmartFeeResponse> estimateSmartFee($grpc.ServiceCall call, $0.EstimateSmartFeeRequest request);
+  $async.Future<$0.EstimateSmartFeeResponse> estimateSmartFee(
+      $grpc.ServiceCall call, $0.EstimateSmartFeeRequest request,);
 }
--- packages/drivechain_client/lib/pages/sidechains_page.dart
+++ packages/drivechain_client/lib/pages/sidechains_page.dart
@@ -1,5 +1,11 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/widgets.dart';
+import 'package:drivechain_client/gen/drivechain/v1/drivechain.pbgrpc.dart';
+import 'package:drivechain_client/pages/send_page.dart';
+import 'package:drivechain_client/providers/sidechain_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:sail_ui/sail_ui.dart';
+import 'package:stacked/stacked.dart';
 
 @RoutePage()
 class SidechainsPage extends StatelessWidget {
@@ -7,6 +13,144 @@ class SidechainsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const QtPage(
+      child: SidechainsView(),
+    );
+  }
+}
+
+class SidechainsView extends StatelessWidget {
+  const SidechainsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return QtContainer(
+      child: ViewModelBuilder.reactive(
+        viewModelBuilder: () => SidechainsViewModel(),
+        builder: (context, model, child) => Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SailText.primary13(
+              'Sidechains',
+              bold: true,
+            ),
+            const SizedBox(height: SailStyleValues.padding15),
+            Expanded(
+              child: ListView.builder(
+                itemCount: model.sidechains.length,
+                itemBuilder: (context, index) {
+                  final sidechain = model.sidechains[index];
+                  final textColor = sidechain == null
+                      ? SailTheme.of(context).colors.textSecondary
+                      : SailTheme.of(context).colors.text;
+
+                  return SelectableListTile(
+                    index: index,
+                    sidechain: sidechain,
+                    textColor: textColor,
+                    isSelected: model.selectedIndex == index,
+                    onSelected: () {
+                      model.toggleSelection(index);
+                    },
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: SailStyleValues.padding15),
+            QtContainer(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  QtButton(
+                    onPressed: () {
+                      showSnackBar(context, 'Not implemented');
+                    },
+                    child: SailText.primary13('Add / Remove'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SelectableListTile extends StatelessWidget {
+  final int index;
+  final ListSidechainsResponse_Sidechain? sidechain;
+  final Color textColor;
+  final bool isSelected;
+  final VoidCallback onSelected;
+
+  const SelectableListTile({
+    required this.index,
+    required this.sidechain,
+    required this.textColor,
+    required this.isSelected,
+    required this.onSelected,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onSelected,
+      child: Container(
+        color: isSelected ? SailTheme.of(context).colors.primary.withOpacity(0.5) : Colors.transparent,
+        child: Row(
+          children: [
+            SizedBox(
+              width: 30,
+              child: SailText.primary13(
+                '$index: ',
+                color: textColor,
+              ),
+            ),
+            SizedBox(
+              width: 80,
+              child: SailText.primary13(
+                sidechain == null ? 'Inactive' : sidechain!.title,
+                color: textColor,
+              ),
+            ),
+            const SailSpacing(SailStyleValues.padding15),
+            SizedBox(
+              width: 120,
+              child: SailText.primary13(
+                sidechain == null ? '' : formatBitcoin(satoshiToBTC(sidechain!.amountSatoshi.toInt())),
+                color: textColor,
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SidechainsViewModel extends BaseViewModel {
+  final SidechainProvider sidechainProvider = GetIt.I.get<SidechainProvider>();
+
+  SidechainsViewModel() {
+    sidechainProvider.addListener(notifyListeners);
+  }
+
+  List<ListSidechainsResponse_Sidechain?> get sidechains => sidechainProvider.sidechains;
+
+  int? _selectedIndex;
+
+  int? get selectedIndex => _selectedIndex;
+
+  void toggleSelection(int index) {
+    if (_selectedIndex == index) {
+      _selectedIndex = null; // Deselect if the same item is selected again
+    } else {
+      _selectedIndex = index; // Select the new item
+    }
+    notifyListeners();
   }
 }
