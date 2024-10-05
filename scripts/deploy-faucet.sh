baa65ae474bbe24fadfc0ba81b46d0f3cd5004fb
Bjørn Olav <bo@jalborg.com>
2024-10-05 17:37:23 +0200

    scripts: update deploy-faucet to be run locally
    the context stuff never works...

--- scripts/deploy-faucet.sh
+++ scripts/deploy-faucet.sh
@@ -2,17 +2,17 @@
 
 set -e 
 
-docker compose --file docker-compose.faucet.yml up --build --wait 
+cd faucet-backend/
+docker build -t faucet-backend .
+cd ../
+docker compose --file docker-compose.faucet.yml up -d --force-recreate
 
-cd ./clients/faucet_client
+cd ./clients/faucet
 flutter clean
 flutter pub get --enforce-lockfile
-flutter build web --profile --dart-define=Dart2jsOptimization=O0
-
-instance=$(gcloud compute instances list --format="value(NAME)" | grep layertwo-labs)
+flutter build web --profile --dart-define=Dart2jsOptimization=O0 --output=./build/web
 
 # Clean old stuff
-gcloud compute ssh $instance -- "rm -rf /var/www/drivechain-live/*"
-
-# Copy over new stuff
-gcloud compute scp --recurse ./build/web/* $instance:/var/www/drivechain-live/
+sudo rm -rf /var/www/drivechain-live/*
+# Move over new stuff
+sudo cp -r ./build/web/* /var/www/drivechain-live/
