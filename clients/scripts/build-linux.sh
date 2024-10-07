07767fc379b378f0b3822513d5f19abf2a9d0ff4
Bjørn Olav <bo@jalborg.com>
2024-10-07 12:54:43 +0200

    clients: move build script into /clients
    builders can call this script to build for various platforms

--- clients/scripts/build-linux.sh
+++ clients/scripts/build-linux.sh
@@ -1,12 +1,20 @@
+#!/usr/bin/env bash
+
 set -e
 
 app_name="$1"
-if [ "$app_name" = "" ]; then
-    echo "Usage: $0 app_name"
+client_dir="$2"
+
+if [ "$app_name" = "" ] || [ "$client_dir" = "" ]; then
+    echo "Usage: $0 app_name client_directory"
     exit 1
 fi
 
 lower_app_name=$(echo "$app_name" | tr '[:upper:]' '[:lower:]')
+
+echo Building $app_name
+
+cd "$client_dir"
 
 flutter build linux --dart-define-from-file=build-vars.env
 
@@ -18,5 +26,6 @@ echo Zipping into $zip_name
 
 zip -q -r $zip_name *
 
-mkdir -p $old_cwd/release
-cp $zip_name $old_cwd/release
+release_dir="$old_cwd/release"
+mkdir -p "$release_dir"
+cp $zip_name "$release_dir"
