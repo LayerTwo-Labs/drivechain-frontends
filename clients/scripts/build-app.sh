ed6b45143787395f8fd19d0faa6cf010664d30e8
Bjørn Olav <bo@jalborg.com>
2024-10-10 06:41:26 +0200

    clients/build: make build-app.sh cleaner, make matrix.os work as parameter

--- clients/scripts/build-app.sh
+++ clients/scripts/build-app.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-# This script orchestrates the build process for clients in the sub-folder of this directory.
-# for different platforms (Linux, macOS, Windows).
-# All sub-folders that want to use this script need these scripts
-# to be present:
-# 1. set-app-name.sh
-# 2. download-binaries.sh
-# 3. flavorize-<linux/macos/windows>.sh
+# This script orchestrates the build process for all clients in this directory, for various
+# OSes (Linux, macOS, Windows).
+# All clients that want to use this script need these scripts:
+# 1. set-app-name.sh - must set the app name and create build-vars.env (if any)
+# 2. download-binaries.sh - responsible for populating the assets/bin directory.
+# 3. flavorize-<linux/macos/windows>.sh - responsible for setting name, icon etc. for the os.
 
 set -e
 
-platform=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+os=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 client="$2"
 chain=$(echo "$3" | tr '[:upper:]' '[:lower:]')
 identity="$4"
@@ -18,69 +17,52 @@ notarization_key_path="$5"
 notarization_key_id="$6"
 notarization_issuer_id="$7"
 
-if test -z "$platform" -o -z "$client"; then
+# Convert github actions os names names to a format we expect
+case "$os" in
+    windows-latest) os="windows" ;;
+    linux-latest)   os="linux" ;;
+    macos-latest)   os="macos" ;;
+esac
+
+if test -z "$os" -o -z "$client"; then
     echo "Usage: $0 <linux/macos/windows> <sidesail/launcher/bitwindow> [chain] [code_sign_identity] [notarization_key_path] [notarization_key_id] [notarization_issuer_id]"
     exit 1
 fi
 
-client_dir="$client"
+old_cwd=$(pwd)
+cleanup() {
+    rm -f "$client_dir/build-vars.env"
+    git checkout -f "$client_dir/macos"
+    git checkout -f "$client_dir/linux"
+    git checkout -f "$client_dir/windows"
+    echo "Cleaned up successfully"
+    cd $old_cwd
+}
+trap cleanup EXIT
 
+client_dir="$client"
 if [ ! -d "$client_dir" ]; then
     echo "Error: Client directory $client_dir does not exist."
     exit 1
 fi
-
-# Enter the directory of the client we're building for
 cd "$client_dir"
 
-cleanup() {
-    rm -f "$client_dir/build-vars.env"
-    echo "Cleaned up successfully"
-    cd ../
-}
-# run cleanup on exit
-trap cleanup EXIT
-
-# set-app-name.sh is required in the client directory. 
-if [ ! -f "./scripts/set-app-name.sh" ]; then
-    echo "set-app-name.sh is required, does not exist in ./$client_dir/scripts/"
-    exit 1
-fi
-
-# Run set-app-name.sh to set $app_name and create build-vars.env
 source ./scripts/set-app-name.sh "$chain"
-
-# Check that set-app-name.sh did its job
 if [ -z "$app_name" ]; then
     echo "Error: set-app-name.sh did not set a valid app name"
     exit 1
 fi
-
-# Print the app_name for debugging
 echo "App name set to: $app_name"
 
-# download-binaries.sh is required in the client directory. 
-if [ ! -f "./scripts/download-binaries.sh" ]; then
-    echo "download-binaries.sh is required, does not exist in ./$client_dir/scripts/"
-    exit 1
-fi
+echo "Downloading $client_dir binaries for $os"
+bash ./scripts/download-binaries.sh "$os" "$chain"
 
-echo "Downloading $client_dir binaries for $platform"
-bash ./scripts/download-binaries.sh "$platform" "$chain"
+echo "Flavorizing $client_dir for $os"
+bash ./scripts/flavorize-"$os".sh "$app_name"
 
-# flavorize-<platform>.sh is required in the client directory. 
-if [ ! -f "./scripts/flavorize-$platform.sh" ]; then
-    echo "Error: flavorize-$platform.sh does not exist in ./$client_dir/scripts/"
-    exit 1
-fi
-
-echo "Flavorizing $client_dir for $platform"
-bash ./scripts/flavorize-"$platform".sh "$app_name"
-
-# Return to the parent directory to run the platform-specific build script
+# Return to the parent directory to run the os-specific build script
 cd ../
-
 echo "Building $client_dir $app_name release"
-bash ./scripts/build-"$platform".sh "$app_name" "$client_dir" \
+bash ./scripts/build-"$os".sh "$app_name" "$client_dir" \
     "$identity" "$notarization_key_path" \
     "$notarization_key_id" "$notarization_issuer_id"
