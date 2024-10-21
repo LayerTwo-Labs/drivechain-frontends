e7c04cba8e7afdc4d117fa6ca09606d844d56853
Bjørn Olav <bo@jalborg.com>
2024-10-21 13:54:32 +0200

    server: refactor get data dir into package

--- drivechain-server/bdk/bdk.go
+++ drivechain-server/bdk/bdk.go
@@ -11,13 +11,13 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
-	"runtime"
 	"slices"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/LayerTwo-Labs/sidesail/drivechain-server/dir"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
@@ -446,45 +446,6 @@ type Transaction struct {
 	TXID     string         `json:"txid"`
 }
 
-func getDataDir() (string, error) {
-	const appName = "bdk-cli"
-	var dir string
-
-	switch runtime.GOOS {
-	case "linux":
-	case "darwin":
-		if xdgDataHome := os.Getenv("XDG_DATA_HOME"); xdgDataHome != "" {
-			dir = filepath.Join(xdgDataHome, appName)
-		} else {
-			home, err := os.UserHomeDir()
-			if err != nil {
-				return "", err
-			}
-			if runtime.GOOS == "darwin" {
-				dir = filepath.Join(home, "Library", "Application Support", appName)
-			} else {
-				dir = filepath.Join(home, ".local", "share", appName)
-			}
-		}
-	case "windows":
-		appData, ok := os.LookupEnv("APPDATA")
-		if !ok {
-			return "", fmt.Errorf("APPDATA environment variable not set")
-		}
-		dir = filepath.Join(appData, appName)
-	default:
-		return "", fmt.Errorf("unsupported OS: %s", runtime.GOOS)
-	}
-
-	// Ensure the directory exists
-	err := os.MkdirAll(dir, 0755)
-	if err != nil && !os.IsExist(err) {
-		return "", err
-	}
-
-	return dir, nil
-}
-
 // NewWallet either creates a new wallet or loads an existing one. If a
 // passphrase is provided, the mnemonic is encrypted before saving if the
 // wallet is created. If the wallet already exists, the passphrase is used to
@@ -492,7 +453,8 @@ func getDataDir() (string, error) {
 func NewWallet(
 	ctx context.Context, network, electrum, passphrase, xprvOverride string,
 ) (*Wallet, error) {
-	datadir, err := getDataDir()
+	const appName = "bdk-cli"
+	datadir, err := dir.GetDataDir(appName)
 	if err != nil {
 		return nil, err
 	}
