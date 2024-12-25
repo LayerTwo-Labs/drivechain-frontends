35a569a9b3ac214bba36c1785f2d0ec698afa87e
Bjørn Olav <bo@jalborg.com>
2024-12-25 09:42:06 +0100

    bitwindowd: build bitwindowd to .exe on windows

--- clients/bitwindow/scripts/download-binaries.sh
+++ clients/bitwindow/scripts/download-binaries.sh
@@ -132,7 +132,13 @@ echo "Building bitwindowd"
 just build-go
 
 # Move the necessary binaries to the assets directory
-mv bin/bitwindowd $assets_dir/
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    echo "moved bin/bitwindowd to $assets_dir/bitwindowd.exe"
+    mv bin/bitwindowd $assets_dir/bitwindowd.exe
+else
+    echo "moved bin/bitwindowd to $assets_dir/bitwindowd"
+    mv bin/bitwindowd $assets_dir/bitwindowd
+fi
 
 echo "bitwindowd with embedded bdk-cli have been built and moved to $assets_dir"
 
