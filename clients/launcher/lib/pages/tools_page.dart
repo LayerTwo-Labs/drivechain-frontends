5dd2ee369c6a1e5b3dc638bbe3382a97de3c1486
seriouscatsoserious <121734574+seriouscatsoserious@users.noreply.github.com>
2025-01-16 12:51:23 -0300

    clients/launcher: add tab system to tools page

--- clients/launcher/lib/pages/tools_page.dart
+++ clients/launcher/lib/pages/tools_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:sail_ui/sail_ui.dart';
+import 'package:stacked/stacked.dart';
 
 @RoutePage()
 class ToolsPage extends StatelessWidget {
@@ -8,15 +9,60 @@ class ToolsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = SailTheme.of(context);
-
-    return Container(
-      color: theme.colors.background,
-      child: Center(
-        child: SailText.primary12(
-          'Tools',
-        ),
+    return QtPage(
+      child: ViewModelBuilder<ToolsPageViewModel>.reactive(
+        viewModelBuilder: () => ToolsPageViewModel(),
+        onViewModelReady: (model) => model.init(),
+        builder: (context, model, child) {
+          return InlineTabBar(
+            tabs: [
+              TabItem(
+                label: 'Fast Withdrawal',
+                icon: SailSVGAsset.iconWithdraw,
+                child: WithdrawalTab(),
+              ),
+              TabItem(
+                label: 'Faucet',
+                icon: SailSVGAsset.iconCoins,
+                child: FaucetTab(),
+              ),
+            ],
+            initialIndex: 0,
+          );
+        },
       ),
     );
+  }
+}
+
+class WithdrawalTab extends ViewModelWidget<ToolsPageViewModel> {
+  const WithdrawalTab({super.key});
+
+  @override
+  Widget build(BuildContext context, ToolsPageViewModel viewModel) {
+    return SailRawCard(
+      title: 'Fast Withdrawal',
+      subtitle: 'Quickly withdraw your funds',
+      child: Container(),
+    );
+  }
+}
+
+class FaucetTab extends ViewModelWidget<ToolsPageViewModel> {
+  const FaucetTab({super.key});
+
+  @override
+  Widget build(BuildContext context, ToolsPageViewModel viewModel) {
+    return SailRawCard(
+      title: 'Faucet',
+      subtitle: 'Get test coins from the faucet',
+      child: Container(),
+    );
+  }
+}
+
+class ToolsPageViewModel extends BaseViewModel {
+  void init() {
+    // Initialize any required data
   }
 }
