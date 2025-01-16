2febe4bd7b074ee07102f8e772c3b7958bd3bf0d
Marc Platt <marcplatt@Marcs-MacBook-Pro.local>
2025-01-16 17:53:20 -0500

    add button to open app data dir as well as reopen welcome modal

--- clients/launcher/lib/pages/settings_page.dart
+++ clients/launcher/lib/pages/settings_page.dart
@@ -1,10 +1,24 @@
+import 'dart:io';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:launcher/env.dart';
+import 'package:launcher/widgets/welcome_modal.dart';
 import 'package:sail_ui/sail_ui.dart';
 
 @RoutePage()
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
+
+  Future<void> _openDataDirectory() async {
+    final appDir = await Environment.datadir();
+    final command = Platform.isMacOS 
+      ? 'open' 
+      : Platform.isWindows 
+        ? 'explorer'
+        : 'xdg-open';
+    
+    await Process.run(command, [appDir.path]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +26,28 @@ class SettingsPage extends StatelessWidget {
 
     return Container(
       color: theme.colors.background,
-      child: Center(
-        child: SailText.primary12(
-          'Settings',
-        ),
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SailText.primary24('Settings'),
+          const SizedBox(height: 24),
+          Row(
+            children: [
+              SailButton.secondary(
+                'Open Data Directory',
+                onPressed: _openDataDirectory,
+                size: ButtonSize.regular,
+              ),
+              const SizedBox(width: 16),
+              SailButton.secondary(
+                'Open Welcome Modal',
+                onPressed: () => showWelcomeModal(context),
+                size: ButtonSize.regular,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
