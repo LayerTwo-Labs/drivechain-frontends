138151f9f9deed200a4dc7f5ce4296b584fe5c8a
Marc Platt <marcplatt@Marcs-MacBook-Pro.local>
2025-01-17 18:31:12 -0500

    update settings for datadir open and modal reopen

--- clients/launcher/lib/pages/settings_page.dart
+++ clients/launcher/lib/pages/settings_page.dart
@@ -1,10 +1,26 @@
+import 'dart:io';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:launcher/env.dart';
+import 'package:launcher/widgets/welcome_modal.dart';
 import 'package:sail_ui/sail_ui.dart';
 
 @RoutePage()
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
+
+  Future<void> _openDataDirectory() async {
+    final appDir = await Environment.datadir();
+    print('Opening directory: ${appDir.path}');
+    
+    if (Platform.isWindows) {
+      await Process.run('explorer', [appDir.path]);
+    } else if (Platform.isMacOS) {
+      await Process.run('open', [appDir.path]);
+    } else if (Platform.isLinux) {
+      await Process.run('xdg-open', [appDir.path]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +28,28 @@ class SettingsPage extends StatelessWidget {
 
     return Container(
       color: theme.colors.background,
-      child: Center(
-        child: SailText.primary12(
-          'Settings',
-        ),
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SailText.primary24('Settings'),
+          const SizedBox(height: 24),
+          Row(
+            children: [
+              SailButton.secondary(
+                'Open Data Directory',
+                onPressed: _openDataDirectory,
+                size: ButtonSize.regular,
+              ),
+              const SizedBox(width: 16),
+              SailButton.secondary(
+                'Open Welcome Modal',
+                onPressed: () => showWelcomeModal(context),
+                size: ButtonSize.regular,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
