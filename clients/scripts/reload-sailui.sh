66bd710c9c0acd915f163529c60b1dbf309d9e80
Bjørn Olav <bo@jalborg.com>
2025-01-18 11:18:34 +0100

    clients: move reload-sailui to clients subfolder

---
+++ clients/scripts/reload-sailui.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+cd sidesail
+flutter pub get
+cd ../
+
+cd faucet
+flutter pub get
+cd ../
+
+cd sail_ui
+flutter pub get
+cd ../
+
+cd launcher
+flutter pub get
+cd ../
+
+cd bitwindow
+flutter pub get
+cd ../
