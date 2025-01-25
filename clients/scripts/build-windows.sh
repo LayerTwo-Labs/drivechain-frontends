43472dd8e60dba4b50e26e6ffff7203d40b39a8d
Bjørn Olav <bo@jalborg.com>
2025-01-25 07:46:59 +0100

    clients: always build zip for gui-binaries

--- clients/scripts/build-windows.sh
+++ clients/scripts/build-windows.sh
@@ -25,15 +25,13 @@ build_cmd="flutter build windows --dart-define-from-file=build-vars.env"
 # Create MSIX with specific options for CI
 msix_cmd="dart run msix:create --build-windows false --install-certificate false"
 
-# Build first
-powershell.exe -Command "& {$clean_cmd; $build_cmd; exit}"
+# Build and create an MSIX
+powershell.exe -Command "& {$clean_cmd; $build_cmd; $msix_cmd; exit}"
 
-# Create MSIX
-powershell.exe -Command "& {$msix_cmd; exit}"
-
-# Create release directory and copy MSIX with fixed name
+# Prepare the release directory with correct names
+msix_name=$lower_app_name.msix
+zip_name=$lower_app_name-win64.zip
 mkdir -p release
 
-# Copy MSIX to release directory and rename it consistently
-msix_name=$lower_app_name-win64.msix
-cp build/windows/x64/runner/Release/*.msix "release/$msix_name"
+# Then zip everything from Release directory including the renamed MSIX
+powershell.exe -Command "Compress-Archive -Force build\windows\x64\runner\Release\* release/$zip_name"
