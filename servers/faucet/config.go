809a49914a1e5c27416125af3084b54528a2f0f7
Torkel Rogstad <torkel@rogstad.io>
2025-02-22 16:02:03 +0100

    faucet-backend: rename CLI opts

--- servers/faucet/config.go
+++ servers/faucet/config.go
@@ -10,14 +10,14 @@ import (
 )
 
 type Config struct {
-	BitcoinCoreHost        string `long:"bitcoincore.host" description:"host:port for connecting to Bitcoin Core" default:"localhost:38332"`
+	BitcoinCoreAddress     string `long:"bitcoincore.address" description:"host:port for connecting to Bitcoin Core" default:"localhost:38332"`
 	BitcoinCoreCookie      string `long:"bitcoincore.cookie" description:"Path to Bitcoin Core cookie file"`
 	BitcoinCoreRPCUser     string `long:"bitcoincore.rpcuser" description:"Username for Bitcoin Core RPC" default:"user"`
 	BitcoinCoreRPCPassword string `long:"bitcoincore.rpcpassword" description:"Password for Bitcoin Core RPC" default:"password"`
 
 	EnforcerHost string `long:"enforcer.host" description:"host:port for connecting to the enforcer server" default:"localhost:50051"`
 
-	APIHost string `long:"api.host" env:"API_HOST" description:"public address for the connect server" default:"localhost:8082"`
+	Listen string `long:"listen" description:"serve API on this address:port" default:"localhost:8082"`
 }
 
 func readConfig() (Config, error) {
