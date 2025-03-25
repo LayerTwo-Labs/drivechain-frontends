fff41152383d81d41e821ee6717cff8f5a23ec4f
Bjørn Olav <bo@jalborg.com>
2025-03-25 11:59:10 +0100

    run scripts/reload-sailui.sh

--- clients/bitwindow/macos/Flutter/GeneratedPluginRegistrant.swift
+++ clients/bitwindow/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import desktop_multi_window
+import file_picker
 import path_provider_foundation
 import screen_retriever_macos
 import url_launcher_macos
@@ -13,6 +14,7 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterMultiWindowPlugin.register(with: registry.registrar(forPlugin: "FlutterMultiWindowPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
