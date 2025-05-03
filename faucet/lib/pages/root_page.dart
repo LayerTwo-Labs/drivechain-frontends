ae9a928082b88a754c7ddfa11226e16c17e09e3d
Bjørn Olav <bo@jalborg.com>
2025-05-03 14:06:07 +0200

    faucet: dont crash frontend

--- faucet/lib/pages/root_page.dart
+++ faucet/lib/pages/root_page.dart
@@ -31,7 +31,7 @@ class RootPage extends StatelessWidget {
               ),
             ],
           ),
-          body: Expanded(child: child),
+          body: child,
         );
       },
     );
