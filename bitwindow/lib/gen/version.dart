f0c0fdc0c214d77009b9110b273f74b1cb9e5a6a
Bjørn Olav <bo@jalborg.com>
2025-08-02 13:06:06 +0200

    ci: add versioning, useful for snap

--- bitwindow/lib/gen/version.dart
+++ bitwindow/lib/gen/version.dart
@@ -1,25 +1,25 @@
 // Generated file - do not edit manually
-// Generated on: 2025-07-30T09:06:10Z
+// Generated on: 2025-08-02T11:05:26Z
 
 class AppVersion {
   /// Short commit hash (7 characters)
-  static const String commit = '7b3cb512';
+  static const String commit = 'c111020c';
   
   /// Full commit hash
-  static const String commitFull = '7b3cb51293256a6ea9eb83f6855bce90c97a412e';
+  static const String commitFull = 'c111020ce966a2fa458d18fec570b7e48aa32cc6';
   
   /// Build date in ISO 8601 format (UTC)
-  static const String buildDate = '2025-07-30T09:06:10Z';
+  static const String buildDate = '2025-08-02T11:05:26Z';
   
   /// Version from pubspec.yaml
-  static const String version = '0.0.1';
+  static const String version = '0.0.3';
   
   /// App name
   static const String appName = 'bitwindow';
   
   /// Combined version string for display
-  static const String versionString = 'v0.0.1 (7b3cb512)';
+  static const String versionString = 'v0.0.3 (c111020c)';
   
   /// Build timestamp as Unix epoch
-  static const String buildTimestamp = '1753866370';
+  static const String buildTimestamp = '1754132726';
 }
