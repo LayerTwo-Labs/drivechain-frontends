21ec3249ecc6b984de893d29c075d3f2cb088084
Bjørn Olav <bo@jalborg.com>
2025-08-26 20:25:47 +0200

    faucet: add info and dev up top

--- faucet/lib/pages/root_page.dart
+++ faucet/lib/pages/root_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:faucet/routing/router.dart';
 import 'package:flutter/material.dart';
 import 'package:sail_ui/sail_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 @RoutePage()
 class RootPage extends StatelessWidget {
@@ -11,10 +12,7 @@ class RootPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AutoTabsRouter.tabBar(
       animatePageTransition: false,
-      routes: [
-        FaucetRoute(),
-        ExplorerRoute(),
-      ],
+      routes: [FaucetRoute(), ExplorerRoute()],
       builder: (context, child, controller) {
         final theme = SailTheme.of(context);
 
@@ -22,12 +20,10 @@ class RootPage extends StatelessWidget {
           backgroundColor: theme.colors.background,
           appBar: TopNav(
             routes: [
-              TopNavRoute(
-                label: 'Faucet',
-              ),
-              TopNavRoute(
-                label: 'Explorer',
-              ),
+              TopNavRoute(label: 'Faucet'),
+              TopNavRoute(label: 'Explorer'),
+              TopNavRoute(label: 'Info', onTap: () => launchUrl(Uri.parse('https://drivechain.info'))),
+              TopNavRoute(label: 'Dev', onTap: () => launchUrl(Uri.parse('https://www.drivechain.info/dev.txt'))),
             ],
           ),
           body: child,
