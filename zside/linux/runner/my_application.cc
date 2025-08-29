83abf8a81703c0df06f4bccce0363ebc33c35969
Bjørn Olav <bo@jalborg.com>
2025-08-29 09:23:28 +0200

    clients/linux: set app icon for all chains

--- zside/linux/runner/my_application.cc
+++ zside/linux/runner/my_application.cc
@@ -1,3 +1,6 @@
+#include <filesystem> 
+using namespace std;
+using namespace std::filesystem;
 #include "my_application.h"
 
 #include <flutter_linux/flutter_linux.h>
@@ -19,6 +22,11 @@ static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+
+  const string iconFilename = "assets/zside.png";
+  path execDir = canonical(read_symlink("/proc/self/exe")).parent_path();
+  path iconPath = execDir / "data/flutter_assets" / iconFilename;
+  gtk_window_set_icon_from_file(GTK_WINDOW(window), iconPath.c_str(), NULL);
 
   // Use a header bar when running in GNOME as this is the common style used
   // by applications and is the setup most users will be using (e.g. Ubuntu
