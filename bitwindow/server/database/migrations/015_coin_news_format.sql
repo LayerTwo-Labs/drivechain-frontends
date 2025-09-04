23746a962af96638185ee0ca918d1f1c9cf449df
Torkel Rogstad <torkel@rogstad.io>
2025-09-04 09:14:45 +0200

    bitwindow: wipe coin news topic, force resync
    We changed the data format! Sorry.

---
+++ bitwindow/server/database/migrations/015_coin_news_format.sql
@@ -0,0 +1,5 @@
+DELETE FROM processed_blocks;
+DELETE FROM coin_news_topics;
+
+INSERT INTO coin_news_topics (topic, name) VALUES ('a1a1a1a1a1a1a1a1', 'US Weekly');
+INSERT INTO coin_news_topics (topic, name) VALUES ('a2a2a2a2a2a2a2a2', 'Japan Weekly');
