c67e22d37b2c98c64a9e208f2ece072d3e46bbba
Bjørn Olav <bo@jalborg.com>
2025-12-06 21:45:10 +0100

    bitassets: add update checking to settings

--- bitassets/lib/pages/tabs/settings_page.dart
+++ bitassets/lib/pages/tabs/settings_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:auto_route/auto_route.dart';
+import 'package:auto_updater/auto_updater.dart';
 import 'package:bitassets/gen/version.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -282,6 +284,33 @@ class _InfoSettingsContent extends StatefulWidget {
 }
 
 class _InfoSettingsContentState extends State<_InfoSettingsContent> {
+  bool _isCheckingForUpdates = false;
+  String? _updateMessage;
+
+  Future<void> _checkForUpdates() async {
+    if (Platform.isLinux) return;
+
+    setState(() {
+      _isCheckingForUpdates = true;
+      _updateMessage = null;
+    });
+
+    try {
+      await autoUpdater.checkForUpdates();
+      setState(() {
+        _updateMessage = 'Check complete. If an update is available, you will be notified.';
+      });
+    } catch (e) {
+      setState(() {
+        _updateMessage = 'Error checking for updates: $e';
+      });
+    } finally {
+      setState(() {
+        _isCheckingForUpdates = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SailColumn(
@@ -335,6 +364,25 @@ class _InfoSettingsContentState extends State<_InfoSettingsContent> {
             SailText.secondary13(AppVersion.appName),
           ],
         ),
+
+        // Updates section (hidden on Linux)
+        if (!Platform.isLinux)
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SailText.primary15('Updates'),
+              const SailSpacing(SailStyleValues.padding08),
+              SailButton(
+                label: 'Check for Updates',
+                loading: _isCheckingForUpdates,
+                onPressed: () async => await _checkForUpdates(),
+              ),
+              if (_updateMessage != null) ...[
+                const SailSpacing(4),
+                SailText.secondary12(_updateMessage!),
+              ],
+            ],
+          ),
       ],
     );
   }
