e1f411d7a4f0421a6d2664a9e1912e47bd94ce3e
Bjørn Olav <bo@jalborg.com>
2026-02-01 08:24:22 +0100

    bitwindow: use correct appdir on windows

--- bitwindow/server/dir/dir.go
+++ bitwindow/server/dir/dir.go
@@ -10,31 +10,37 @@ import (
 func DefaultDataDir() (string, error) {
 
 	var dir string
-	const appName = "bitwindow"
 
 	switch runtime.GOOS {
 	case "linux":
+		const linuxAppName = "bitwindow"
+
 		if xdgDataHome := os.Getenv("XDG_DATA_HOME"); xdgDataHome != "" {
-			dir = filepath.Join(xdgDataHome, appName)
+			dir = filepath.Join(xdgDataHome, linuxAppName)
 		} else {
 			home, err := os.UserHomeDir()
 			if err != nil {
 				return "", err
 			}
-			dir = filepath.Join(home, ".local", "share", appName)
+			dir = filepath.Join(home, ".local", "share", linuxAppName)
 		}
 	case "darwin":
+		const macosAppName = "bitwindow"
+
 		home, err := os.UserHomeDir()
 		if err != nil {
 			return "", err
 		}
-		dir = filepath.Join(home, "Library", "Application Support", appName)
+		dir = filepath.Join(home, "Library", "Application Support", macosAppName)
 	case "windows":
+		const windowsAppName = "BitWindow"
+
 		appData, ok := os.LookupEnv("APPDATA")
 		if !ok {
 			return "", fmt.Errorf("APPDATA environment variable not set")
 		}
-		dir = filepath.Join(appData, appName)
+		// Match Flutter's path_provider which uses CompanyName\ProductName from Runner.rc
+		dir = filepath.Join(appData, "10520LayertwoLabs", windowsAppName)
 	default:
 		return "", fmt.Errorf("unsupported OS: %s", runtime.GOOS)
 	}
