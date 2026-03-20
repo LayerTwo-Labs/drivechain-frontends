74c47e849693e26dbde2e2b88d7e866e794cc85f
Bjørn Olav <bo@jalborg.com>
2026-03-20 07:18:33 +0100

    coinshift: update pubspec.lock

--- scripts/reload-sailui.sh
+++ scripts/reload-sailui.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-for dir in thunder bitnames bitassets zside bitassets photon truthcoin bitwindow; do
+for dir in thunder bitnames bitassets zside bitassets photon truthcoin bitwindow coinshift; do
   (
     cd "$dir"
     flutter pub get
   ) &
 done
 
-for dir in thunder bitnames bitassets zside bitassets photon truthcoin bitwindow; do
+for dir in thunder bitnames bitassets zside bitassets photon truthcoin bitwindow coinshift; do
   (
     cd "$dir/macos"
     pod install --repo-update
