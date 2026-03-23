c820ae79cf815524b0ae28fbe6c3b86d26aa9816
Bjørn Olav <bo@jalborg.com>
2026-03-23 21:32:42 +0100

    sail_ui: increase article modal height to 90%

--- sail_ui/lib/widgets/modals/article_info_modal.dart
+++ sail_ui/lib/widgets/modals/article_info_modal.dart
@@ -12,7 +12,7 @@ void showArticleDetails(BuildContext context, Article article, String groupTitle
     backgroundColor: theme.colors.backgroundSecondary,
     useSafeArea: true,
     constraints: BoxConstraints(
-      maxHeight: MediaQuery.of(context).size.height * 0.6,
+      maxHeight: MediaQuery.of(context).size.height * 0.9,
       maxWidth: 700,
     ),
     builder: (context) {
