9d2cce37560fb5d22d4c9a8a403c12076884f51c
octobocto <bo@jalborg.com>
2026-05-22 06:05:53 -0700

    sail_ui: modernize SailSpacing constructor

--- sail_ui/lib/widgets/spacing.dart
+++ sail_ui/lib/widgets/spacing.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class SailSpacing extends StatelessWidget {
   final double spacing;
-  const SailSpacing(this.spacing, [Key? key]) : super(key: key);
+  const SailSpacing(this.spacing, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(width: spacing, height: spacing);
+    return SizedBox.square(dimension: spacing);
   }
 }
