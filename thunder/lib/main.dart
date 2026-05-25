ebe4db9331c82fd56a7255cdd62c324c2f83e516
octobocto <bo@jalborg.com>
2026-05-25 09:51:29 -0700

    thunder: drop material import from non-root files

--- thunder/lib/main.dart
+++ thunder/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:auto_updater/auto_updater.dart';
 import 'package:collection/collection.dart';
 import 'package:thunder/gen/version.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
+// ignore: avoid_material_import
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
