29cd77761520810b3bdb7af702a97d25828e4850
octobocto <bo@jalborg.com>
2026-05-25 11:39:31 -0700

    truthcoin: add StepState to narrowed material import

--- truthcoin/lib/pages/market_creation_page.dart
+++ truthcoin/lib/pages/market_creation_page.dart
@@ -1,5 +1,5 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart' show Colors, Step, Stepper;
+import 'package:flutter/material.dart' show Colors, Step, Stepper, StepState;
 import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sail_ui/sail_ui.dart';
