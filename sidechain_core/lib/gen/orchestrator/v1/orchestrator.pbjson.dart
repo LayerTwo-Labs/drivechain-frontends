15609c615a4634becf7f76d59a5c4858b26b92be
Bjørn Olav <bo@jalborg.com>
2026-09-16 20:37:11 +0200

    orchestrator: report the migration start time

--- sidechain_core/lib/gen/orchestrator/v1/orchestrator.pbjson.dart
+++ sidechain_core/lib/gen/orchestrator/v1/orchestrator.pbjson.dart
@@ -1434,6 +1434,7 @@ const ECashMigrationStatus$json = {
     {'1': 'pruned', '3': 18, '4': 1, '5': 8, '10': 'pruned'},
     {'1': 'prune_height', '3': 19, '4': 1, '5': 3, '10': 'pruneHeight'},
     {'1': 'wallet_only', '3': 20, '4': 1, '5': 8, '10': 'walletOnly'},
+    {'1': 'started_at_unix', '3': 21, '4': 1, '5': 3, '10': 'startedAtUnix'},
   ],
 };
 
@@ -1450,7 +1451,7 @@ final $typed_data.Uint8List eCashMigrationStatusDescriptor = $convert.base64Deco
     'Cghjb21wbGV0ZRgPIAEoCFIIY29tcGxldGUSFAoFZXJyb3IYECABKAlSBWVycm9yEh0KCnN5bm'
     'Nfc3RhdGUYESABKAlSCXN5bmNTdGF0ZRIWCgZwcnVuZWQYEiABKAhSBnBydW5lZBIhCgxwcnVu'
     'ZV9oZWlnaHQYEyABKANSC3BydW5lSGVpZ2h0Eh8KC3dhbGxldF9vbmx5GBQgASgIUgp3YWxsZX'
-    'RPbmx5');
+    'RPbmx5EiYKD3N0YXJ0ZWRfYXRfdW5peBgVIAEoA1INc3RhcnRlZEF0VW5peA==');
 
 const $core.Map<$core.String, $core.dynamic> OrchestratorServiceBase$json = {
   '1': 'OrchestratorService',
